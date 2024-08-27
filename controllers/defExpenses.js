677ca30a27f1bbb8c81ba8d6d7ee965483f89748
Govind M J <govindmj2002@gmail.com>
2024-08-27 10:03:07 +0530

    updated def controller

--- controllers/defExpenses.js
+++ controllers/defExpenses.js
@@ -27,8 +27,28 @@ const addDefExpense = async (req, res) => {
 
 const getAllDefExpensesByTruckId = async (req, res) => {
     try {
-        const { truckId } = req.params;
-        const defExpenses = await DefExpense.find({ truckId });
+        const { truckId } = req.query;
+        if (!truckId) {
+            return res.status(400).json({ message: 'Truck ID is required' });
+        }
+        const defExpenses = await DefExpense.find({ truckId }).sort({ date: 1 });
+        if (defExpenses.length === 0) {
+            return res.status(404).json({ message: 'No def expenses found for this truck' });
+        }
+        const formattedDefExpenses = defExpenses.map((expense, index) => {
+            // Format the date to 'YYYY-MM-DD'
+            const date = new Date(expense.date);
+            const formattedDate = date.toISOString().split('T')[0];
+
+            // Calculate mileage
+            const range = index > 0 ? expense.currentKM - fuelExpenses[index - 1].currentKM : 0;
+
+            return {
+                ...expense.toObject(), // Convert Mongoose document to plain object
+                date: formattedDate,
+                range
+            };
+        });
         res.status(200).json(defExpenses);
     } catch (error) {
         console.error('Error retrieving def expenses:', error);
